14b3075eae022f49568fb5be93c262aff655251b
rvalikov <r.valikov@mail.ru>
2023-02-17 22:02:01 +0300

    Доделал ввод пользователя

--- index.js
+++ index.js
@@ -1,87 +1,94 @@
-import express  from 'express'
-// import ExcelJS from 'exceljs'
-import { PORT, TOKEN } from './config.js'
-import TelegramBot from 'node-telegram-bot-api'; // подключаем node-telegram-bot-api
+import express from 'express';
+import TelegramBot from 'node-telegram-bot-api';
+import fs from 'fs';
+import { readExcel } from './telegram-excel-bot.js';
+import { PORT, TOKEN } from './config.js';
 import { KeyboardAgree, MakeKeyboard } from './keyboard.js';
-import fs from "fs"
-import  { readExcel}  from './telegram-excel-bot.js';
 
-const bot = new TelegramBot(TOKEN, {polling:true});
-const app = express()
-const keyboard=MakeKeyboard()
-const keyboardAgree=KeyboardAgree()
+const bot = new TelegramBot(TOKEN, { polling: true });
+const app = express();
+const keyboard = MakeKeyboard();
+const keyboardAgree = KeyboardAgree();
+let employee = {}
+let count = 1
 
+// Обработчик команды /start
+bot.onText(/\/start/, (msg) => {
+  count = 1;
+  employee = {};
+  console.log("countbegin", count)
+  const chatId = msg.chat.id;
+  const chatName = msg.chat.first_name;
+  
 
-// GREETING BLOCK
-bot.onText(/\/start/, msg => {
-    const chatId = msg.chat.id; //получаем идентификатор диалога, чтобы отвечать именно тому пользователю, который нам что-то прислал
-    console.log(chatId)
-  const chatName= msg.chat.first_name
-    // отправляем сообщение
-    bot.sendMessage(chatId, `Привет ${chatName}!  Познакомимся? `,
-    // include keyboard
-    {
-        reply_markup: {
-            inline_keyboard: keyboard
-        }
+  bot.sendMessage(chatId, `Привет ${chatName}!  Познакомимся? `, {
+    reply_markup: {
+      inline_keyboard: keyboard
     }
-    )
-    })
-   //Обработчик нажатия клавиатуры
-    bot.on('callback_query', (query) => {
-        const chatId = query.message.chat.id; //получаем идентификатор диалога, чтобы отвечать именно тому пользователю, который нам что-то прислал
-     
-        // отправляем сообщение
-        switch(query.data){
-            case "yes":{ bot.sendMessage(chatId, `Загрузка согласия`)
-            fs.readFile("./Согласие по ФЗ-152.pdf",(err,file)=>{
-                bot.sendDocument(chatId, file,
-                    {
-                        caption:"Согласие"
-                    })
-                .then(()=>{bot.sendMessage(chatId, `Подпиши согласие`,
-                 // include keyboard
-            {
-                reply_markup: {
-                    inline_keyboard: keyboardAgree
-                }
-            } )
-            })
+  });
+});
+
+// Обработчик нажатия кнопок
+bot.on('callback_query', (query) => {
+  const chatId = query.message.chat.id;
+
+  switch (query.data) 
+  {
+    case 'yes':
+      bot.sendMessage(chatId, `Загрузка согласия`);
+      fs.readFile('./Согласие по ФЗ-152.pdf', (err, file) => {
+        bot.sendDocument(chatId, file, {
+          caption: 'Согласие'
         })
-            
-            break;
-             }
-             case "yesAgree":{
-                        
-                bot.sendMessage(chatId, `Согласие подписано. Введите Ваше Имя Фамилию Отдел Телефон через пробел`)
-                bot.on("message", async (msg) => {
-                    const lastName = msg.chat.last_name
-                    const chatId = msg.chat.id;
-                    const text = msg.text;
-                    const textlist = text.split(" ");
-                  
-                    bot.sendMessage(chatId, `Ваши данные ${textlist}`);
-                  
-                    const flag = await readExcel(textlist);
-                  
-                    if (flag) {
-                        const AdminId= 590783190
-                        bot.sendMessage(AdminId, `Пользователь  найден в таблице Excel!`)
-                        
-                        
-                    } else {const AdminId= 590783190
-                        bot.sendMessage(AdminId,'Пользователь не найден в таблице Excel.')
-                    }
-                  });
-                  break;  
-          }
+          .then(() => {
+            bot.sendMessage(chatId, `Подпиши согласие`, {
+              reply_markup: {
+                inline_keyboard: keyboardAgree
+              }
+            });
+          });
+      });
+      break;
+
+    case 'yesAgree':
+      bot.sendMessage(chatId, `Согласие подписано. введите имя`);
+      
+      // Обработчик введенных данных
+      bot.on('message', (msg) => {
+        if (count == 1) {
+          employee.firstName = msg.text;
+          count++;
+          bot.sendMessage(chatId, 'Введите фамилию сотрудника:');
+        } else if (count == 2) {
+          employee.lastName = msg.text;
+          count++;
+          bot.sendMessage(chatId, 'Введите отдел сотрудника:');
+        } else if (count == 3) {
+          employee.department = msg.text;
+          count++;
+          bot.sendMessage(chatId, 'Введите телефон сотрудника:');
+        } else if (count == 4) {
+          employee.phone = msg.text;
+          bot.sendMessage(chatId, `Вы ввели: Имя: ${employee.firstName}, Фамилия: ${employee.lastName}, Отдел: ${employee.department}, Телефон: ${employee.phone}`);
+          
+        }
+      
+    })
+
+break;
+  
+                   
                  
-             
+                  
+                
+                   
+     
              case "noAgree":{
                 bot.sendMessage(chatId, `К сожалению, без подписания Согласия мы не можем взаимодействовать`)
                    break     
              }
-            }
-    })
+  }
+})
+   
 // bot.launch()
 app.listen(PORT, () => console.log(`My server is running on port ${PORT}`))
--- telegram-excel-bot.js
+++ telegram-excel-bot.js
@@ -1,6 +1,6 @@
 import ExcelJS from "exceljs"
 
-export function readExcel(textlist) {
+export function readExcel(employee) {
   const workbook = new ExcelJS.Workbook();
   let flag = false;
 
@@ -9,30 +9,31 @@ export function readExcel(textlist) {
       console.log('Excel file read successfully.');
 
       const worksheet = workbook.getWorksheet('Лист1');
-      const user = {
-        firstName: textlist[0].toUpperCase(),
-        lastName: textlist[1].toUpperCase(),
-        department: textlist[2].toUpperCase(),
-        phone: textlist[3],
-      }
+      // const user = {
+      //   firstName: textlist[0].toUpperCase(),
+      //   lastName: textlist[1].toUpperCase(),
+      //   department: textlist[2].toUpperCase(),
+      //   phone: textlist[3],
+      // }
 
       worksheet.eachRow((row, rowNumber) => {
         const rowValues = row.values;
-
-        if (rowValues[1].toUpperCase() == user.firstName &&
-            rowValues[2].toUpperCase() == user.lastName &&
-            rowValues[3].toUpperCase() == user.department &&
-            rowValues[4] == user.phone) {
+console.log("Проверка")
+        if (rowValues[1].toUpperCase() == employee.firstName &&
+            rowValues[2].toUpperCase() == employee.lastName &&
+            rowValues[3].toUpperCase() == employee.department &&
+            rowValues[4] == employee.phone) {
           flag = true;
         }
       });
 
       if (flag) {
-        console.log(`Пользователь ${user.lastName} найден в таблице Excel!`);
+        
+        console.log(`Пользователь ${employee.lastName} найден в таблице Excel!`);
       } else {
-        console.log('Пользователь не найден в таблице Excel.');
+        console.log(`Пользователь ${employee.lastName} не найден в таблице Excel.`);
       }
-
+      
       return flag;
     })
     .catch((error) => {
